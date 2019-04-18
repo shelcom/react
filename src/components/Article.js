4d8b366287298c9754b61e2ec6fe7c2c1039772b
shelcom <shelcom96@gmail.com>
2019-04-19 01:07:06 +0300

    lesson5

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-	const {article} = props
-    const body = <section>{article.text}</section>
-    return (
-        <div>
-        	<h2>{article.title}</h2>
-        	{body}
-        </div>
-    )
+class Article extends Component {
+	
+	state = {
+		isOpen: true
+	}
+
+	render() {
+		const {article} = this.props
+    	const body = this.state.isOpen && <section>{article.text}</section>
+    	return (
+        	<div>
+        		<h2>{article.title}</h2>
+        		<button onClick={this.handleClick}>
+        		{this.state.isOpen ? 'close' : 'open'}
+        		</button>
+        		{body}
+        	</div>
+        )	
+    }
+
+
+	handleClick = () => {
+		console.log('---', 'clicked')
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
 }
-
 export default Article 
